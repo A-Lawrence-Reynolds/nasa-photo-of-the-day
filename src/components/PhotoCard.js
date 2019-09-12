089ff7b9bb5a7ef13fb50a6d21f6b64403afe358
Adam-Reynolds <a-lawrence-reynolds@protonmail.com>
2019-09-12 15:48:34 -0700

    Added styled components, dose not look great but I understand how all the importing and how all the suff under the hood is working

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,14 +1,46 @@
 import React from "react";
+import styled from "styled-components";
+const Photo = styled.div`
+  font-family: "Righteous", cursive;
+  color: #f2c861;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const SpaceImage = styled.img`
+  // height: %;
+  width: 95%;
+  object-fit: scale;
+  flex-shrink: 2;
+`;
+
+const HeaderText = styled.h1`
+  font-size: 15rem;
+  text-shadow: grey 1px 1px 2pc;
+`;
+
+const Desc = styled.h2`
+  width: 90%;
+  backgound: grey;
+  border-radius: 5px;
+`;
+
+const Title = styled.h3``;
+const Date = styled.h3``;
 
 const PhotoCard = props => {
   return (
-    <div className="photo" key={props.photo.url}>
-      <h1 className="header-text">NASA photo of the day</h1>
-      <img className="photo-img" alt="space photo" src={props.photo.hdurl} />
-      <h2 className="">{props.photo.explanation}</h2>
-      <h3>{props.photo.title}</h3>
-      <h4>{props.photo.date}</h4>
-    </div>
+    <Photo key={props.photo.url}>
+      <HeaderText>NASA photo of the day</HeaderText>
+      <SpaceImage
+        className="photo-img"
+        alt="space photo"
+        src={props.photo.hdurl}
+      />
+      <Desc>{props.photo.explanation}</Desc>
+      <Title>{props.photo.title}</Title>
+      <Date>{props.photo.date}</Date>
+    </Photo>
   );
 };
 
